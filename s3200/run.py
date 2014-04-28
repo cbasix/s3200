2899b2e31e9558bb8c95b6b762a88e9803162b05
cbasix <githubsf@blackgear.de>
2014-04-28 20:26:13 +0200

    Remote Debugging

---
+++ s3200/run.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+#import pydevd
+#pydevd.settrace('192.168.17.103', port=8880, stdoutToServer=True, stderrToServer=True)
+from s3200 import constants
+from s3200.obj import S3200
+
+if __name__ == '__main__':
+
+    s = S3200('dummy',
+              value_definitions=constants.VALUE_DEFINITIONS,
+              value_group_definitions=constants.VALUE_GROUP_DEFINITIONS)
+    print("T:"+str(s.get_errors()))
